6c954fa361421314447e99654613ff40af46f1aa
ivaivaiva1 <120752841+ivaivaiva1@users.noreply.github.com>
2026-04-09 08:23:14 -0300

    bug fix

--- scripts/Effects/bubble.gd
+++ scripts/Effects/bubble.gd
@@ -1,10 +1,24 @@
-extends Node
+extends Node2D
 
 @onready var sprite: AnimatedSprite2D = %Sprite
+@onready var auto_destruction_position: float = randf_range(145, 150)
+var is_alive: bool = true
+
+
+func _process(delta: float) -> void:
+	if !is_alive: return
+	if global_position.y < auto_destruction_position:
+		destroy_bubble()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
+	if !is_alive: return
 	if(area.is_in_group("bait")):
-		sprite.play("kabum")
+		destroy_bubble()
 	
 	if(area.is_in_group("shark")):
-		sprite.play("kabum")
+		destroy_bubble()
+
+
+func destroy_bubble():
+	is_alive = false
+	sprite.play("kabum")
