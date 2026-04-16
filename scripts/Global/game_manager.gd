201071a5ac6f690f49e53ebabbf622916be808d3
ivaivaiva1 <120752841+ivaivaiva1@users.noreply.github.com>
2026-04-16 01:12:28 -0300

    fix: UI bugs

--- scripts/Global/game_manager.gd
+++ scripts/Global/game_manager.gd
@@ -4,21 +4,20 @@ class_name GameManager
 var game_state: String = "menu"
 @onready var FishSpawner_intance: FishSpawner = %Fish_Spawner
 @onready var Shark: Shark = %shark
-@onready var press_to_play: Label = %press_to_play
 @export var player_scene: PackedScene
 var player1: Player
 var player2: Player
 
-@onready var placar: Label = %PLACAR
+@onready var rounds_player1_label: Label = %player1_rounds
+@onready var rounds_player2_label: Label = %player2_rounds
 @onready var money_1_label: Label = %player1_money
 @onready var money_2_label: Label = %player2_money
 @export var peso_1: Label 
 @export var peso_2: Label 
 @onready var price_flutuation: PriceFlutuation = %PriceFlutuation
 @onready var clone_boats: CloneBoats = %CloneBoats
-@onready var duration_bar: ProgressBar = %ProgressBar
 #var round_duration: float = 240
-var round_duration: float = 240
+var round_duration: float = 30
 var game_timer: float = 0
 var _time_accumulator: float = 0.0
 @onready var treasure_scene: PackedScene = load("res://scenes/treasure.tscn")
@@ -31,21 +30,21 @@ func _ready() -> void:
 	FishSpawner_intance.spawn_cooldown = GlobalVars.spawn_cooldown_menu
 	FishSpawner_intance.big_fish_rate = GlobalVars.big_fish_rate_menu
 	FishSpawner_intance.red_fish_rate = GlobalVars.red_fish_rate_menu
-	duration_bar.max_value = round_duration
-	duration_bar.value = round_duration - game_timer
+	#duration_bar.max_value = round_duration
+	#duration_bar.value = round_duration - game_timer
 	EffectSpawner.fish_spawner = FishSpawner_intance
 	att_score()
 	start_game()
 
 
 func _process(delta: float) -> void:
-	peso_1.text = "peso: " + str(GlobalVars.player1_peso)
-	peso_2.text = "peso: " + str(GlobalVars.player2_peso)
+	#peso_1.text = "peso: " + str(GlobalVars.player1_peso)
+	#peso_2.text = "peso: " + str(GlobalVars.player2_peso)
 	if game_state == "game":
 		game_timer += delta
 	
 	
-	duration_bar.value = round_duration - game_timer
+	#duration_bar.value = round_duration - game_timer
 	if game_timer > round_duration: 
 		finish_round()
 	
@@ -84,7 +83,7 @@ func start_game():
 	
 	FishSpawner_intance.remove_all_fish()
 	Shark.re_start()
-	press_to_play.visible = false
+	#press_to_play.visible = false
 	money_1_label.visible = true
 	money_2_label.visible = true
 		#peso_1.visible = true
@@ -125,7 +124,9 @@ func att_money():
 
 
 func att_score():
-	placar.text = str(GlobalVars.player1_score , "           " , GlobalVars.player2_score)
+	rounds_player1_label.text = str(GlobalVars.player1_score)
+	rounds_player2_label.text = str(GlobalVars.player2_score)
+
 
 
 func spawn_treasure():
