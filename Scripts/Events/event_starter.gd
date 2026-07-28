b0e9276a9aa2f6a36a69c5cae45390095208912a
Leandro Cezar <lcacampos@gmail.com>
2026-07-28 16:48:27 -0300

    feat: new splash screen

--- Scripts/Events/event_starter.gd
+++ Scripts/Events/event_starter.gd
@@ -37,7 +37,7 @@ func spawn_event():
 			sort_number = false
 	
 	var big_cooldown: bool = true
-	event_index = 3
+	# event_index = 3
 	match event_index:
 		0:
 			banished_events.append(event_index)
